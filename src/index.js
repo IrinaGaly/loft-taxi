befc0c2281bfe0391db1b93b0bc8231922198e3c
Irina Galygina <irina.galygina@enrian.com>
2021-04-02 19:14:40 +0200

    add map and authenticate method for login

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { theme } from "loft-taxi-mui-theme";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import { AuthProvider } from './AuthContext';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MuiThemeProvider theme={theme}>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,98 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { PropTypes } from 'prop-types'
 import { Input } from './../components/Input'
+import { withAuth } from "./../AuthContext";
 
-export const Login = ({navigate}) => {
-  return (
-    <form className="login__form">
-      <h2 className="login__title">Войти</h2>
-      <Input name="email" id="email" label="Email" placeholder="Введите email" myClass="login__label" />
-      <Input name="password" id="password" label="Пароль" placeholder="Введите пароль" myClass="login__label" />
-      <button className="login__reset-password">Забыли пароль?</button>
-      <input type="submit" className="login__submit" value="Войти" onClick={() => {navigate("map")}} />
-      <div className="login__signup-wrapper"><span className="login__new-user">Новый пользователь?</span><button className="login__loginup" onClick={() => {navigate("registration")}} >Регистрация</button></div>
-    </form>
-  )
+// export const Login = ({navigate}) => {
+//   return (
+//     <form className="login__form">
+//       <h2 className="login__title">Войти</h2>
+//       <Input name="email" id="email" label="Email" placeholder="Введите email" myClass="login__label" />
+//       <Input name="password" id="password" label="Пароль" placeholder="Введите пароль" myClass="login__label" />
+//       <button className="login__reset-password">Забыли пароль?</button>
+//       <input type="submit" className="login__submit" value="Войти" onClick={() => {navigate("map")}} />
+//       <div className="login__signup-wrapper"><span className="login__new-user">Новый пользователь?</span><button className="login__loginup" onClick={() => {navigate("registration")}} >Регистрация</button></div>
+//     </form>
+//   )
+// }
+
+export class Login extends Component {
+  goToProfile = (event) => {
+    event.preventDefault();
+    this.props.navigate("profile");
+  };
+
+  goToRegistration = (event) => {
+    event.preventDefault();
+    this.props.navigate("registration");
+  };
+
+  authenticate = (event) => {
+    event.preventDefault();
+    const { email, password } = event.target;
+    this.props.logIn(email.value, password.value);
+  };
+
+  render() {
+    return (
+      // <>
+      //   {
+      //   this.props.isLoggedIn ? (
+      //     <p>
+      //       You are logged in{" "}
+      //       <button onClick={this.goToProfile}>
+      //         go to profile
+      //       </button>
+      //     </p>
+      //   ) : (
+      //     <form className="login__form">
+      //      <h2 className="login__title">Войти</h2>
+      //      <Input name="email" id="email" label="Email" placeholder="Введите email" myClass="login__label" />
+      //      <Input name="password" id="password" label="Пароль" placeholder="Введите пароль" myClass="login__label" />
+      //      <button className="login__reset-password">Забыли пароль?</button>
+      //      <input type="submit" className="login__submit" value="Войти" onClick={() => {this.navigate("map")}} />
+      //      <div className="login__signup-wrapper"><span className="login__new-user">Новый пользователь?</span><button className="login__loginup" onClick={() => {this.navigate("registration")}} >Регистрация</button></div>
+      //    </form>
+      //  )
+      //   }
+      // </>
+        // this.props.isLoggedIn ? (
+        //   <p>
+        //     You are logged in{" "}
+        //     <button onClick={this.goToProfile}>
+        //       go to profile
+        //     </button>
+        //   </p>
+        // ) :
+         
+          // <form onSubmit={this.authenticate}>
+          //   <label htmlFor="email">Email:</label>
+          //   <input id="email" type="email" name="email" size="28" />
+          //   <label htmlFor="password">Password:</label>
+          //   <input id="password" type="password" name="password" size="28" />
+          //   <button type="submit" onClick={this.props.isLoggedIn ? this.goToProfile : null }>Log in</button>
+          // </form>
+      <>
+        {
+          (
+          <form onSubmit={this.authenticate} className="login__form">
+            <h2 className="login__title">Войти</h2>
+            <Input name="email" id="email" label="Email" placeholder="Введите email" myClass="login__label" />
+            <Input name="password" id="password" label="Пароль" placeholder="Введите пароль" myClass="login__label" />
+            <button className="login__reset-password">Забыли пароль?</button>
+            <button type="submit" onClick={this.props.isLoggedIn ? this.goToProfile : null } className="login__submit">Войти</button>
+            <div className="login__signup-wrapper"><span className="login__new-user">Новый пользователь?</span><button className="login__loginup" onClick={!this.props.isLoggedIn ? this.goToRegistration : null }>Регистрация</button></div>
+          </form>
+        )}
+      </>
+    );
+  }
 }
+
+Login.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logIn: PropTypes.func,
+  navigateTo: PropTypes.func,
+};
+
+export const LoginWithAuth = withAuth(Login);
